ac2d2fd1d136dd0a4b3225b0adf16cdfcbee2898
Machin Max <machin.max@laplateforme.io>
2024-02-06 11:34:54 +0100

    switch crud class

---
+++ src/Class/Crud.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Class;
+
+class Crud
+{
+    private $dbConnexion;
+
+    public function __construct() 
+    {
+    }
+
+    public function Create(){
+
+    }
+
+    public function GetAll(){
+    
+    }
+
+    public function getByAttributes(array $attributes){
+
+    }
+
+    public function Delete($id){
+
+    }
+
+    public function Update($id){
+
+    }
+}
