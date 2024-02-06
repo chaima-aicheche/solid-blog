8f55e08c60a13a1212df52ba05252609f7bf4b0a
Machin Max <machin.max@laplateforme.io>
2024-02-07 00:08:39 +0100

    prepare post branche

--- src/Class/Post.php
+++ src/Class/Post.php
@@ -229,109 +229,109 @@ class Post
         ];
     }
 
-    public function save()
-    {
-        if (empty($this->id)) {
-            $this->insert();
-        } else {
-            $this->update();
-        }
-    }
+    // public function save()
+    // {
+    //     if (empty($this->id)) {
+    //         $this->insert();
+    //     } else {
+    //         $this->update();
+    //     }
+    // }
 
-    public function findOneById(int $id): self
-    {
-        $connection = Database::getConnection();
-        $query = $connection->prepare('SELECT * FROM post WHERE id = :id');
-        $query->bindValue(':id', $id, \PDO::PARAM_INT);
-        $query->execute();
-        $arrayPost = $query->fetch(\PDO::FETCH_ASSOC);
-        $this->id = $arrayPost['id'];
-        $this->title = $arrayPost['title'];
-        $this->content = $arrayPost['content'];
-        $this->createdAt = new DateTime($arrayPost['created_at']);
-        $this->updatedAt = $arrayPost['updated_at'] ? new DateTime($arrayPost['updated_at']) : null;
-        $this->user = (new User())->findOneById($arrayPost['user_id']);
-        $this->category = (new Category())->findOneById($arrayPost['category_id']);
-        $this->comments = (new Comment())->findByPost($arrayPost['id']);
-        return $this;
-    }
+    // public function findOneById(int $id): self
+    // {
+    //     $connection = Database::getConnection();
+    //     $query = $connection->prepare('SELECT * FROM post WHERE id = :id');
+    //     $query->bindValue(':id', $id, \PDO::PARAM_INT);
+    //     $query->execute();
+    //     $arrayPost = $query->fetch(\PDO::FETCH_ASSOC);
+    //     $this->id = $arrayPost['id'];
+    //     $this->title = $arrayPost['title'];
+    //     $this->content = $arrayPost['content'];
+    //     $this->createdAt = new DateTime($arrayPost['created_at']);
+    //     $this->updatedAt = $arrayPost['updated_at'] ? new DateTime($arrayPost['updated_at']) : null;
+    //     $this->user = (new User())->findOneById($arrayPost['user_id']);
+    //     $this->category = (new Category())->findOneById($arrayPost['category_id']);
+    //     $this->comments = (new Comment())->findByPost($arrayPost['id']);
+    //     return $this;
+    // }
 
-    public function findAll()
-    {
-        $connection = Database::getConnection();
-        $query = $connection->prepare('SELECT * FROM post');
-        $query->execute();
-        $arrayPosts = $query->fetchAll(\PDO::FETCH_ASSOC);
-        $results = [];
-        foreach ($arrayPosts as $arrayPost) {
-            $post = new Post();
-            $post->setId($arrayPost['id']);
-            $post->setTitle($arrayPost['title']);
-            $post->setContent($arrayPost['content']);
-            $post->setCreatedAt(new DateTime($arrayPost['created_at']));
-            $post->setUpdatedAt($arrayPost['updated_at'] ? new DateTime($arrayPost['updated_at']) : null);
-            $post->setUser((new User())->findOneById($arrayPost['user_id']));
-            $post->setCategory((new Category())->findOneById($arrayPost['category_id']));
-            $post->setComments((new Comment())->findByPost($arrayPost['id']));
-            $results[] = $post;
-        }
-        return $results;
-    }
+    // public function findAll()
+    // {
+    //     $connection = Database::getConnection();
+    //     $query = $connection->prepare('SELECT * FROM post');
+    //     $query->execute();
+    //     $arrayPosts = $query->fetchAll(\PDO::FETCH_ASSOC);
+    //     $results = [];
+    //     foreach ($arrayPosts as $arrayPost) {
+    //         $post = new Post();
+    //         $post->setId($arrayPost['id']);
+    //         $post->setTitle($arrayPost['title']);
+    //         $post->setContent($arrayPost['content']);
+    //         $post->setCreatedAt(new DateTime($arrayPost['created_at']));
+    //         $post->setUpdatedAt($arrayPost['updated_at'] ? new DateTime($arrayPost['updated_at']) : null);
+    //         $post->setUser((new User())->findOneById($arrayPost['user_id']));
+    //         $post->setCategory((new Category())->findOneById($arrayPost['category_id']));
+    //         $post->setComments((new Comment())->findByPost($arrayPost['id']));
+    //         $results[] = $post;
+    //     }
+    //     return $results;
+    // }
 
-    public function insert()
-    {
-        $connection = Database::getConnection();
-        $query = $connection->prepare('INSERT INTO post (title, content, user_id, category_id, created_at) VALUES (:title, :content, :user_id, :category_id, NOW())');
-        $query->bindValue(':title', $this->title, \PDO::PARAM_STR);
-        $query->bindValue(':content', $this->content, \PDO::PARAM_STR);
-        $query->bindValue(':user_id', $this->user->getId(), \PDO::PARAM_INT);
-        $query->bindValue(':category_id', $this->category->getId(), \PDO::PARAM_INT);
-        $query->execute();
-        $this->id = $connection->lastInsertId();
-    }
+    // public function insert()
+    // {
+    //     $connection = Database::getConnection();
+    //     $query = $connection->prepare('INSERT INTO post (title, content, user_id, category_id, created_at) VALUES (:title, :content, :user_id, :category_id, NOW())');
+    //     $query->bindValue(':title', $this->title, \PDO::PARAM_STR);
+    //     $query->bindValue(':content', $this->content, \PDO::PARAM_STR);
+    //     $query->bindValue(':user_id', $this->user->getId(), \PDO::PARAM_INT);
+    //     $query->bindValue(':category_id', $this->category->getId(), \PDO::PARAM_INT);
+    //     $query->execute();
+    //     $this->id = $connection->lastInsertId();
+    // }
 
-    public function update()
-    {
-        $connection = Database::getConnection();
-        $query = $connection->prepare('UPDATE post SET title = :title, content = :content, user_id = :user_id, category_id = :category_id, updated_at = NOW() WHERE id = :id');
-        $query->bindValue(':id', $this->id, \PDO::PARAM_INT);
-        $query->bindValue(':title', $this->title, \PDO::PARAM_STR);
-        $query->bindValue(':content', $this->content, \PDO::PARAM_STR);
-        $query->bindValue(':user_id', $this->user->getId(), \PDO::PARAM_INT);
-        $query->bindValue(':category_id', $this->category->getId(), \PDO::PARAM_INT);
-        $query->execute();
-    }
+    // public function update()
+    // {
+    //     $connection = Database::getConnection();
+    //     $query = $connection->prepare('UPDATE post SET title = :title, content = :content, user_id = :user_id, category_id = :category_id, updated_at = NOW() WHERE id = :id');
+    //     $query->bindValue(':id', $this->id, \PDO::PARAM_INT);
+    //     $query->bindValue(':title', $this->title, \PDO::PARAM_STR);
+    //     $query->bindValue(':content', $this->content, \PDO::PARAM_STR);
+    //     $query->bindValue(':user_id', $this->user->getId(), \PDO::PARAM_INT);
+    //     $query->bindValue(':category_id', $this->category->getId(), \PDO::PARAM_INT);
+    //     $query->execute();
+    // }
 
-    public function delete()
-    {
-        $connection = Database::getConnection();
-        $query = $connection->prepare('DELETE FROM post WHERE id = :id');
-        $query->bindValue(':id', $this->id, \PDO::PARAM_INT);
-        $query->execute();
-    }
+    // public function delete()
+    // {
+    //     $connection = Database::getConnection();
+    //     $query = $connection->prepare('DELETE FROM post WHERE id = :id');
+    //     $query->bindValue(':id', $this->id, \PDO::PARAM_INT);
+    //     $query->execute();
+    // }
 
-    public function findByUser(User $user)
-    {
-        $sql = 'SELECT * FROM post WHERE user_id = :user_id';
-        $stmt = Database::getConnection()->prepare($sql);
-        $stmt->bindValue(':user_id', $user->getId(), \PDO::PARAM_INT);
-        $stmt->execute();
-        $results = [];
-        $arrayPost = $stmt->fetchAll(\PDO::FETCH_ASSOC);
-        foreach ($arrayPost as $arrayPost) {
-            $post = new Post();
-            $post->setId($arrayPost['id']);
-            $post->setTitle($arrayPost['title']);
-            $post->setContent($arrayPost['content']);
-            $post->setCreatedAt(new DateTime($arrayPost['created_at']));
-            $post->setUpdatedAt($arrayPost['updated_at'] ?? new DateTime($arrayPost['updated_at']));
-            $post->setUser((new User())->findOneById($arrayPost['user_id']));
-            $post->setCategory((new Category())->findOneById($arrayPost['category_id']));
-            $post->setComments((new Comment())->findByPost($arrayPost['id']));
-            $results[] = $post;
-        }
-        return $results;
-    }
+    // public function findByUser(User $user)
+    // {
+    //     $sql = 'SELECT * FROM post WHERE user_id = :user_id';
+    //     $stmt = Database::getConnection()->prepare($sql);
+    //     $stmt->bindValue(':user_id', $user->getId(), \PDO::PARAM_INT);
+    //     $stmt->execute();
+    //     $results = [];
+    //     $arrayPost = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    //     foreach ($arrayPost as $arrayPost) {
+    //         $post = new Post();
+    //         $post->setId($arrayPost['id']);
+    //         $post->setTitle($arrayPost['title']);
+    //         $post->setContent($arrayPost['content']);
+    //         $post->setCreatedAt(new DateTime($arrayPost['created_at']));
+    //         $post->setUpdatedAt($arrayPost['updated_at'] ?? new DateTime($arrayPost['updated_at']));
+    //         $post->setUser((new User())->findOneById($arrayPost['user_id']));
+    //         $post->setCategory((new Category())->findOneById($arrayPost['category_id']));
+    //         $post->setComments((new Comment())->findByPost($arrayPost['id']));
+    //         $results[] = $post;
+    //     }
+    //     return $results;
+    // }
 
     public function findAllPaginated(int $page)
     {
